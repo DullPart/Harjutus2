63333532d09434f4bdd56c950415e8bc289d9722
Tehnikumi Õpilane <opilane@tptlive.ee>
2016-10-19 16:55:15 +0300

    Initial commit

---
+++ src/Date.java
@@ -0,0 +1,10 @@
+/**
+ * Created by opilane on 19.10.2016.
+ */
+public class Date {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        Dates.dates();
+    }
+}
---
+++ src/Dates.java
@@ -0,0 +1,17 @@
+/**
+ * Created by opilane on 19.10.2016.
+ */
+public class Dates {
+    public static void dates() {
+        String day = "Wednesday ";
+        String date = "19th ";
+        String month = "October ";
+        int year = 2016;
+
+        System.out.println(day + date + month + year);
+        System.out.println(date);
+        System.out.println(month);
+        System.out.println(year);
+
+    }
+}
